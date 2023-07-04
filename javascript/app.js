7284803b563cb0e84f48836c69f706a04e7267cb
Anas Ahmed <123072865+anasahmed2@users.noreply.github.com>
2023-07-04 23:41:55 +0500

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,385 @@
+var mobilesData = {
+    iphone: {
+        iphone14Model1: {
+            model: "IPHONE 14 PRO MAX 256GB",
+            price: 5_99_999,
+            images: "images/ip-14-pro-b-1.jpg",
+            conpany: "iPhone"
+        },
+        iphone14Model2: {
+            model: "IPHONE 14 PRO MAX 256GB",
+            price: 6_10_000,
+            images: "images/ip-14-pro-b-2.jpg",
+            conpany: "iPhone"
+        },
+        iphone14Model3: {
+            model: "IPHONE 14 PRO MAX 256GB",
+            price: 5_80_000,
+            images: "images/ip-14-pro-b-3.jpg",
+            conpany: "iPhone"
+        },
+        iphone14Model4: {
+            model: "IPHONE 14 PRO MAX 256GB",
+            price: 5_50_000,
+            images: "images/ip-14-pro-b-4.jpg",
+            conpany: "iPhone"
+        },
+        iphone13Model1: {
+            model: "IPHONE 13 PRO MAX 256GB",
+            price: 3_20_000,
+            images: "images/ip-13-pro-b-1.jpg",
+            conpany: "iPhone"
+        },
+        iphone13Model2: {
+            model: "IPHONE 13 PRO MAX 256GB",
+            price: 3_00_000,
+            images: "images/ip-13-pro-b-2.jpg",
+            conpany: "iPhone"
+        },
+        iphone12Model1: {
+            model: "IPHONE 12 PRO MAX 256GB",
+            price: 190_00_0,
+            images: "images/ip-12-pro-b-1.jpg",
+            conpany: "iPhone"
+        },
+        iphone12Model2: {
+            model: "IPHONE 12 PRO MAX 256GB",
+            price: 2_50_000,
+            images: "images/ip-12-pro-b-2.jpg",
+            conpany: "iPhone"
+        },
+        iphone12Model3: {
+            model: "IPHONE 12 PRO MAX 256GB",
+            price: 2_10_000,
+            images: "images/ip-12-pro-b-3.jpg",
+            conpany: "iPhone"
+        },
+        iphone12Model4: {
+            model: "IPHONE 12 PRO MAX 128GB",
+            price: 1_32_000,
+            images: "images/ip-12-pro-b-4.jpg",
+            conpany: "iPhone"
+        },
+        iphone11Model1: {
+            model: "IPHONE 11 PRO 256GB",
+            price: 170_00_0,
+            images: "images/ip-11-pro-b-1.jpg",
+            conpany: "iPhone"
+        },
+        iphone11Model2: {
+            model: "IPHONE 11 PRO 128GB",
+            price: 115_00_0,
+            images: "images/ip-11-pro-b-2.jpg",
+            conpany: "iPhone"
+        },
+        iphone11Model3: {
+            model: "IPHONE 11 PRO 256GB",
+            price: 132_00_0,
+            images: "images/ip-11-pro-b-3.jpg",
+            conpany: "iPhone"
+        },
+        iphoneXModel1: {
+            model: "IPHONE XS 64GB",
+            price: 59_00_0,
+            images: "images/ip-x-b-1.jpg",
+            conpany: "iPhone"
+        },
+        iphoneXModel2: {
+            model: "IPHONE X 256GB",
+            price: 90_00_0,
+            images: "images/ip-x-b-2.jpg",
+            conpany: "iPhone"
+        },
+        iphoneXModel3: {
+            model: "IPHONE Xsmax 256gb",
+            price: 10_000_0,
+            images: "images/ip-x-b-3.jpg",
+            conpany: "iPhone"
+        },
+        iphone7Model1: {
+            model: "IPHONE 7 PLUS 128GB",
+            price: 49_500,
+            images: "images/ip-7-b-1.jpg",
+            conpany: "iPhone"
+        },
+        iphone7Model2: {
+            model: "IPHONE 7 PLUS 128GB",
+            price: 49_500,
+            images: "images/ip-7-b-2.jpg",
+            conpany: "iPhone"
+        },
+    },
+    android: {
+        iChargerModel1: {
+            model: "Infinix Note 12 Silver 4/128GB",
+            price: 54_999,
+            images: "images/ad-1-b.jpg",
+            conpany: "android"
+        },
+        iChargerModel2: {
+            model: "Realme C35 4/128GB",
+            price: 32_900,
+            images: "images/ad-2-b.jpg",
+            conpany: "android"
+        },
+        iChargerMode3: {
+            model: "tecno CAMON 19 NEO 4/128GB",
+            price: 43_899,
+            images: "images/ad-3-b.jpg",
+            conpany: "android"
+        },
+        iChargerModel3: {
+            model: "Samsung Galaxy S23 Ultra ",
+            price: 5_60_000,
+            images: "images/ad-4-b.jpg",
+            conpany: "android"
+        },
+    },
+    gagde: {
+        iWatchModel1: {
+            model: "A8 Ultra Smart Watch in Apple Logo | 49 MM | Dual Straps",
+            price: 7_600,
+            images: "images/ip-watch-1.jpg",
+            conpany: "iPhone"
+        },
+        iWatchModel2: {
+            model: "Smart Watch i7 Pro",
+            price: 4_300,
+            images: "images/ip-watch-2.jpg",
+            conpany: "iPhone"
+        },
+        iWatchModel3: {
+            model: "SMART WATCH IW7",
+            price: 5_200,
+            images: "images/ip-watch-3.jpg",
+            conpany: "iPhone"
+        },
+        iWatchModel4: {
+            model: "SMART WATCH GS8 MAX (SERIES 8 COPY)",
+            price: 6_600,
+            images: "images/ip-watch-4.jpg",
+            conpany: "iPhone"
+        },
+        iWatchModel5: {
+            model: "Z66 SMARTWATCH",
+            price: 4_000,
+            images: "images/ip-watch-5.jpg",
+            conpany: "iPhone"
+        },
+        ipodModel1: {
+            model: "EARPODS PRO (2ND GENERATION)",
+            price: 4_000,
+            images: "images/ip-air-b-1.jpg",
+            conpany: "iPhone"
+        },
+        ipodModel2: {
+            model: "EARPODS 3",
+            price: 3_300,
+            images: "images/ip-air-b-2.jpg",
+            conpany: "iPhone"
+        },
+        ipodModel3: {
+            model: "Apple Original Handsfree",
+            price: 3_100,
+            images: "images/ip-air-b-3.jpg",
+            conpany: "iPhone"
+        },
+        ipodModel4: {
+            model: "EARPODS PRO BLACK",
+            price: 3_500,
+            images: "images/ip-air-b-4.jpg",
+            conpany: "iPhone"
+        },
+        iChargerModel1: {
+            model: "Apple 20W Charger + CABLE",
+            price: 6_100,
+            images: "images/ip-charge-b-1.jpg",
+            conpany: "iPhone"
+        },
+        iChargerModel2: {
+            model: "APPLE 5W ORIGINAL CHARGER + CABLE",
+            price: 3_100,
+            images: "images/ip-charge-b-2.jpg",
+            conpany: "iPhone"
+        },
+        iChargerModel3: {
+            model: "Apple 20W Charger Merchantile",
+            price: 7_600,
+            images: "images/ip-charge-b-3.jpg",
+            conpany: "iPhone"
+        },
+    }
+
+}
+
+
+var modal = document.getElementById("modal")
+var catagries = document.getElementById("catagries")
+var mobileModelArea = document.getElementById("mobileModel")
+var viewAll = document.getElementById("view-all")
+var mobileLogoModal = document.getElementById("mobile-logo-modal")
+var cardIphn = document.getElementById("card-iphone")
+var cardGdts = document.getElementById("card-gagde")
+var cardadrd = document.getElementById("card-android")
+var btnIphn = document.getElementById("btnIphone")
+var btnGdts = document.getElementById("btngadgets")
+var btnAdrd = document.getElementById("btnAndriod")
+
+
+viewAll.addEventListener("click", function () {
+    catagries.style.display = "none"
+    modal.style.display = "none"
+    mobileLogoModal.style.display = "block"
+    mobileLogoModal.innerHTML = `
+    <img src="images/apple-lodo-black.png" alt="" width="80px">`
+    allProductShow()
+})
+
+function allProductShow() {
+    mobileModelArea.innerHTML = "";
+    for (var key in mobilesData) {
+        for (var key2 in mobilesData[key]) {
+            mobileModelArea.innerHTML += `
+        <div class="all-mobiles animate__animated animate__zoomIn"><img src="${mobilesData[key][key2].images}" alt="" width="100%">
+        <div class="model-data">
+        <p class="model-h">${mobilesData[key][key2].model}</p>
+        <P class="model-star">
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+    </P>
+    <P class="model-rs">RS${mobilesData[key][key2].price}.00</P>
+    <button class="model-card-btn">Add To Card</button>
+</div>
+</div>`
+        }
+    }
+
+}
+allProductShow()
+
+// navbar button //
+btnIphn.addEventListener("click", function () {
+    modal.style.display = "none"
+    catagries.style.display = "none"
+    mobileLogoModal.style.display = "block"
+    mobileLogoModal.innerHTML = `
+    <img src="images/apple-lodo-black.png" alt="" width="80px">`
+    filtergenerate("iphone")
+})
+btnGdts.addEventListener("click", function () {
+    modal.style.display = "none"
+    catagries.style.display = "none"
+    mobileLogoModal.style.display = "block"
+    mobileLogoModal.innerHTML = `
+    <img src="images/apple-lodo-black.png" alt="" width="80px">`
+    filtergenerate("gagde")
+})
+btnAdrd.addEventListener("click", function () {
+    modal.style.display = "none"
+    catagries.style.display = "none"
+    mobileLogoModal.style.display = "block"
+    mobileLogoModal.innerHTML = `
+    <img src="images/android-lodo.png" alt="" width="80px">`
+    filtergenerate("android")
+})
+
+// card function //
+
+cardIphn.addEventListener("click", function () {
+    modal.style.display = "none"
+    catagries.style.display = "none"
+    mobileLogoModal.style.display = "block"
+    mobileLogoModal.innerHTML = `
+    <img src="images/apple-lodo-black.png" alt="" width="80px">`
+    filtergenerate("iphone")
+})
+cardGdts.addEventListener("click", function () {
+    modal.style.display = "none"
+    catagries.style.display = "none"
+    mobileLogoModal.style.display = "block"
+    mobileLogoModal.innerHTML = `
+    <img src="images/apple-lodo-black.png" alt="" width="80px">`
+    console.log("hello")
+    filtergenerate("gagde")
+})
+cardadrd.addEventListener("click", function () {
+    filtergenerate("android")
+    modal.style.display = "none"
+    catagries.style.display = "none"
+    mobileLogoModal.style.display = "block"
+    mobileLogoModal.innerHTML = `
+    <img src="images/android-lodo.png" alt="" width="80px">`
+    console.log("hello")
+})
+
+function filtergenerate(brands) {
+    mobileModelArea.innerHTML = "";
+    for (var key in mobilesData) {
+        if (brands === key) {
+            break;
+        }
+
+    }
+    for (var key2 in mobilesData[key]) {
+
+        mobileModelArea.innerHTML += `
+        <div class="all-mobiles animate__animated animate__zoomIn"><img src="${mobilesData[key][key2].images}" alt="" width="100%">
+        <div class="model-data">
+        <p class="model-h">${mobilesData[key][key2].model}</p>
+        <P class="model-star">
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+    </P>
+    <P class="model-rs">RS${mobilesData[key][key2].price}.00</P>
+    <button class="model-card-btn">Add To Card</button>
+</div>
+</div>`
+    }
+}
+
+var btnIphone14 = document.getElementById("iphone14")
+
+btnIphone14.addEventListener("click", function () {
+    iphone14("IPHONE 14")
+    mobileLogoModal.style.display = "block"
+    mobileLogoModal.innerHTML = `
+    <img src="images/apple-lodo-black.png" alt="" width="80px">`
+    modal.style.display = "none"
+    catagries.style.display = "none"
+    console.log("hello")
+})
+function iphone14(finds) {
+
+    mobileModelArea.innerHTML = "";
+    for (var key in mobilesData) {
+        for (var key2 in mobilesData[key]) {
+            if (mobilesData[key][key2].model.slice(0, 9) === finds) {
+                console.log(mobilesData[key][key2].model.slice(0, 9))
+                mobileModelArea.innerHTML += `
+                <div class="all-mobiles animate__animated animate__zoomIn"><img src="${mobilesData[key][key2].images}" alt="" width="100%">
+                <div class="model-data">
+            <p class="model-h">${mobilesData[key][key2].model}</p>
+            <P class="model-star">
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            </P>
+            <P class="model-rs">RS${mobilesData[key][key2].price}.00</P>
+            <button class="model-card-btn">Add To Card</button>
+            </div>
+            </div>`
+            }
+            console.log("hello")
+        }
+        break;
+    }
+
+}
